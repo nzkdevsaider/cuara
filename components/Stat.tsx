cb1eee1efe5dd8039ae201b325f2ef50e2a5f6b7
Sebastián <sebasmoraresu@gmail.com>
2024-03-28 04:38:13 -0500

    Actualizar estilos en el componente Stat

--- components/Stat.tsx
+++ components/Stat.tsx
@@ -1,27 +1,41 @@
-import { View } from "react-native-ui-lib";
+import { View, StyleSheet } from "react-native";
 
-const Stat = ({
-  children,
-  horizontal,
-  vertical,
-}: {
-  children: React.ReactNode;
-  horizontal?: boolean;
-  vertical?: boolean;
-}) => {
-  return (
-    <View
-      className={`bg-white p-4 shadow-lg ${
-        horizontal
-          ? "flex-row justify-between items-center"
-          : vertical
-          ? "flex-col justify-center"
-          : "flex-row"
-      } w-full rounded-lg`}
-    >
-      {children}
-    </View>
-  );
+const Stat = ({ children, horizontal, vertical }: StatProps) => {
+  const containerStyle = [
+    styles.container,
+    horizontal && styles.horizontal,
+    vertical && styles.vertical,
+  ];
+
+  return <View style={containerStyle}>{children}</View>;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 16,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.15,
+    shadowRadius: 0.40,
+    elevation: 2,
+    borderRadius: 15,
+    width: "100%",
+    flexDirection: "row",
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  vertical: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default Stat;
