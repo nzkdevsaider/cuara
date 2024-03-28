36ff7cb72ffebb761ae4d0d7203af84371f6a01e
Sebastián <sebasmoraresu@gmail.com>
2024-03-28 04:35:06 -0500

    Agrega estilos y contenedores al componente de inicio de sesión

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,49 @@
-import { Image, View } from "react-native";
+import { Image, View, StyleSheet } from "react-native";
 import Login from "../components/Login";
 import { supabase } from "../utils/supabase";
 import { useEffect, useState } from "react";
 import { Session } from "@supabase/supabase-js";
 import { router } from "expo-router";
+
 const index = () => {
   const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
     if (session?.user) return router.push("/home");
-
     supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session);
     });
-
     supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session);
     });
   }, [session]);
 
   return (
-    <View className="flex flex-col justify-center w-full m-auto p-4">
-      <View className="my-4">
-        <Image
-          source={require("../assets/logo.png")}
-          style={{ width: 200, height: 200, resizeMode: "contain" }}
-          className="m-auto"
-        />
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image source={require("../assets/logo.png")} style={styles.logo} />
       </View>
       <Login />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+  },
+  logoContainer: {
+    marginVertical: 16,
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+    alignSelf: "center",
+  },
+});
 
 export default index;
