870874f8419adde836afa62240b6373f1843b251
Sebastián <sebasmoraresu@gmail.com>
2024-03-30 03:36:21 -0500

    Agregado archivo colors.ts con definiciones de colores

---
+++ utils/colors.ts
@@ -0,0 +1,11 @@
+export const Color = {
+  text: {
+    primary: "#51566b",
+    secondary: "#7d7b98",
+    tertiary: "#404040",
+  },
+  button: {
+    primary: "#8e6b95",
+    secondary: "#febd59",
+  },
+};
