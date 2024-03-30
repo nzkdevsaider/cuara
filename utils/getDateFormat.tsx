b28754b482f3ce2035e16674826bf8f27ac3c4d3
Sebastián <sebasmoraresu@gmail.com>
2024-03-30 03:36:41 -0500

    Eliminar función getFechaFormateada en getDateFormat.tsx

--- utils/getDateFormat.tsx
+++
@@ -1,7 +0,0 @@
-import { format, parse } from "date-fns";
-import { es } from "date-fns/locale";
-
-export function getFechaFormateada(fecha: string) {
-  const fechaParsed = parse(fecha, "yyyy-MM-dd", new Date());
-  return format(fechaParsed, "MMMM dd", { locale: es });
-}
